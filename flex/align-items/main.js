035c53fdff4dbaa7a12597af4a8ed6d266f53c1e
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-18 11:57:08 +0530

    code splitting and reuse

--- flex/align-items/main.js
+++
@@ -1,5 +0,0 @@
-var r = document.querySelector(":root");
-
-function changeProperty(value) {
-  r.style.setProperty("--property", value);
-}
--- flex/main.js
+++ flex/main.js
@@ -1,8 +1,5 @@
 var r = document.querySelector(':root');
 
 function changeProperty(value){
-  let prev = r.style.getPropertyValue('--property');
-  r.style.setProperty('--prev', prev);
   r.style.setProperty('--property', value);
-
 }
